33b8b04e54bcdfb5899e81a65ac67622bd01c982
Ujwal <ujwal99arak@gmail.com>
2020-06-21 00:03:55 +0530

    replaced button css with styled component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
-
-import './styles.css';
+const ButtonWrapper = styled.button`
+  padding: 0.5rem 2rem;
+  color: white;
+  border: none;
+  outline:none;
+  font-weight: 600;
+  border-radius: 4px;
+  text-align: center;
+  text-transform: uppercase;
+  background-image: linear-gradient(62deg, #8EC5FC 0%, #9BE2D6 100%);
+`;
 
 const Button = ({children, ...props}) => (
-  <button {...props} className="btn">
+  <ButtonWrapper {...props} className="btn">
     {children}
-  </button>
+  </ButtonWrapper>
 );
 
 export default Button;
